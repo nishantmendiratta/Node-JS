93cee8d73402e958a1d77128d5dfe82d80a03509
Nishant Mendiratta <nishuarora.nishant@gmail.com>
2014-03-15 16:08:36 +0530

    Server accessing custom module

---
+++ my_module.js
@@ -0,0 +1,6 @@
+function hello(){
+	return "world";
+}
+
+//Explicitly declaring objectes to be used 
+module.exports.hello = hello;
---
+++ server_accessing_my_module.js
@@ -0,0 +1,3 @@
+/*Accessing function*/
+var myModule = require("./my_module.js");
+console.log("My module message : " + myModule.hello());
