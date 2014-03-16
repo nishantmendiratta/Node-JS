d01906b0ca4dd3552595c23d26852dc146afb59c
Nishant Mendiratta <nishuarora.nishant@gmail.com>
2014-03-16 12:42:54 +0530

    GIT API module

---
+++ github_module.js
@@ -0,0 +1,49 @@
+/* For GIT we need to use https */
+var https = require("https");
+
+//Wrapping this code into a function to reuse it for different user and adding callback as it takes time to get response
+function getRepos(username , callback){
+	/* We have to pass some options into the request,Creating a seperate object for that*/
+	var options = {
+		host: 'api.github.com',
+		path: '/users/' + username + '/repos', // From API Documentation http://developer.github.com/v3/ || Goto-> Repositories -> List user repositories 
+		method: 'GET',
+		//Request requires user-agent
+		headers: { 
+	    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0'
+		}
+	};
+
+	/* Create our request and get response */
+	var request = https.request(options,function (response){
+
+		var body = '';
+		//Getting data in chunks and concatinating it to create data object (JSON)
+		response.on("data",function(chunk){
+			body+= chunk.toString('utf8');
+		});
+
+		response.on("end",function(){
+			//Create a new array to get response 
+			var repos = [];
+
+			var json = JSON.parse(body);
+			
+			json.forEach(function(repo){
+				repos.push({
+					name: repo.name,
+					description: repo.description
+				});
+			});
+
+			//Passing result after receiving request
+			callback(repos);
+
+		});
+	});
+
+	//To finish our request 
+	request.end();	
+}
+
+module.exports.github = getRepos;
--- server_accessing_my_module.js
+++ server_accessing_my_module.js
@@ -3,3 +3,9 @@ var myModule = require("./my_module.js");
 console.log("First module message : " + myModule.helloworld());
 console.log("Second module message : " + myModule.helloworldagain());
 console.log("Third module message : " + myModule.privatefunctionincrement(1));
+
+var git_module = require("./github_module.js");
+
+git_module.github("nishantmendiratta",function(repos){
+	console.log("Nishant Mendiratta repos",repos);
+});
