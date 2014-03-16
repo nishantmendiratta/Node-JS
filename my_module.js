37b3eb3cc36740b8815de1460d41fb48dfd21603
Nishant Mendiratta <nishuarora.nishant@gmail.com>
2014-03-16 12:36:24 +0530

    Creating private modules

--- my_module.js
+++ my_module.js
@@ -2,5 +2,18 @@ function hello(){
 	return "world";
 }
 
-//Explicitly declaring objectes to be used 
-module.exports.hello = hello;
+function helloAgain(){
+	return hello() + " again";
+}
+
+function myPrivateFunction(number){
+	return number + 1;
+}
+
+function increment(number){
+	return myPrivateFunction(number);
+}
+//Explicitly declaring objectes to be used publically
+module.exports.helloworld = hello;
+module.exports.helloworldagain = helloAgain;
+module.exports.privatefunctionincrement = increment;
--- server_accessing_my_module.js
+++ server_accessing_my_module.js
@@ -1,3 +1,5 @@
 /*Accessing function*/
 var myModule = require("./my_module.js");
-console.log("My module message : " + myModule.hello());
+console.log("First module message : " + myModule.helloworld());
+console.log("Second module message : " + myModule.helloworldagain());
+console.log("Third module message : " + myModule.privatefunctionincrement(1));
