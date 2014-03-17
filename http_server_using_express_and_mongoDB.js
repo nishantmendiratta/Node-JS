b233e5fcba3a49c5ce05681211905f2aa162850f
Nishant Mendiratta <nishuarora.nishant@gmail.com>
2014-03-17 15:02:07 +0530

    Using express with mongoDB for reading data dynamically with server

--- http_server_using_express_and_mongoDB.js
+++ http_server_using_express_and_mongoDB.js
@@ -9,6 +9,15 @@ var config = JSON.parse(fs.readFileSync("config_host.json"));
 var host = config.host;
 var port = config.port;
 
+//Including the namespace we still have to access classes
+var mongo = require("mongodb");
+
+//Choose our host and port
+var dbHost = "127.0.0.1";
+//Get port from Connection (listening information) 
+var dbPort = mongo.Connection.DEFAULT_PORT;
+
+
 /* Including express framework */
 var express = require("express");
 
@@ -32,27 +41,49 @@ app.get("/hello/:text",function (request,response){
 	response.send(request.params.text);
 });
 
-
-var users = {
-	"1":{
-		"name":"nishant mendiratta",
-		"twitter":"Mrmendiratta"
-	},
-	"2":{
-		"name":"blood connection",
-		"twitter":"bloodkonnection"
-	}
-};
-
 /* Using ID to search for page and then request user specific page */
 app.get("/user/:id",function (request,response){
-	var user = users[request.params.id];
-	if(user){
-		response.send("<a href='http://twitter.com/"+user.twitter+"'>Follow " + user.name + " on twitter</a>");
-	}else{
-		response.send("Can't find user :(",404);
-	}
+	
+	getUser(request.params.id,function(user){
+		if(!user){
+			response.send("Can't find user :(",404);
+		}else{
+			response.send("<a href='http://twitter.com/"+user.twitter+"'>Follow " + user.name + " on twitter</a>");
+		}
+	});
 });
 
 /* Similar to http callback */
 app.listen(port,host); 
+
+//Wrapping this into a search function 
+function getUser(id,callback){
+	//Database variable which will store our connection details
+	//mongo is connection Db is the class and node-js-intro is name of database
+	//Then we need to pass the server to connect to then we will use mongo namespace and class called Server
+	var db = new mongo.Db("node-js-intro",new mongo.Server(dbHost,dbPort,{}) );
+
+	//open the connection. This accepts one argument that is callback 
+	db.open(function(error){
+		console.log("We are connected to : " + dbHost + ":" + dbPort);
+		//Specify collection - similar to a table RDBMS, user table
+		db.collection("user",function(error,collection){
+			console.log("we have the collection");
+			//user collection is ready to use 
+			//Searching (cursor is for iterating over the results)
+			//Specifying the column we are looking at (id)
+			collection.find({"id":id.toString()},function(error,cursor){
+				//Save result in normal javascript array
+				cursor.toArray(function(error,users){
+					if(users.length==0){
+						//No user found!
+						callback(false);
+					}else{
+						//Found user,Returning result
+						callback(users[0]);
+					}
+				});
+			});
+		});
+	});
+}
